49cf0b9dbc6a451f2fac3e6e0bb28549bb8af66f
Himanshu kumar <kh419368@gmail.com>
2025-07-26 23:29:52 +0530

    Restore test.js with initial content and comments for variables and datatypes

---
+++ js-chaiaurcode/test.js
@@ -0,0 +1,47 @@
+console.log("Himanshu");
+// // variables
+// const accountId=234678;
+// var accountName="ace123";
+// let accountEmail="abc@gmail.com";
+// console.log(accountId);
+// // accountId=2341;
+// // console.log(accountId);
+// console.log(accountName);
+// accountName="ram2323";
+// console.log(accountName);
+// console.log(accountEmail);
+// accountEmail="Laila@123";
+// console.log(accountEmail);
+// console.table({accountId, accountName, accountEmail});
+
+// //datatypes
+// let name="Himanshu";
+// let age=20;
+// let isloggedIn=false;
+
+
+// let score=22;
+// console.log(typeof score);
+// console.log(score);
+
+// let no="45";
+// console.log(typeof no);
+// let number=Number(no);
+// let st=String(no);
+// let boo=Boolean(no);
+// console.log(typeof number);
+// console.log(number);
+// console.log(st);
+// console.log(boo);
+
+// let x=2;
+// console.log(x);
+// x--;
+// console.log(x);
+// x++;
+// console.log(x);
+
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+
