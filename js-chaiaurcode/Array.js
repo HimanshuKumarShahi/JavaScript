5606e3549970312cef092dc79fc389870f3156b6
Himanshu kumar <kh419368@gmail.com>
2025-08-05 23:24:21 +0530

    Refactor Array.js: Comment out unused name array and enhance array method examples

--- js-chaiaurcode/Array.js
+++ js-chaiaurcode/Array.js
@@ -1,4 +1,12 @@
 const arr=[2,4,5,6];
-const name=["ram","sita","laxman","bharat"]
+// const name=["ram","sita","laxman","bharat"]
 console.log(arr[0]); 
-console.log(name[0],name[3]); 
+// console.log(name[0],name[3]); 
+
+// Array methods
+arr.push(234);
+arr.push(999);
+arr.pop();
+arr.unshift(9);
+arr.shift();
+console.log(arr); 
