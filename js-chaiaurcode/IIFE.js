03c9b715ad9e687a50f670647c785f257207ac65
Himanshu kumar Shahi <himanshushahi2006@gmail.com>
2025-08-23 22:00:25 +0530

    learn IIFE
    fast execution theprgram

---
+++ js-chaiaurcode/IIFE.js
@@ -0,0 +1,9 @@
+(function chai(){
+    console.log("DB connected .....");
+    
+})();
+
+(()=>(console.log("DB connecting ......!")))();
+
+
+((name)=>(console.log(`${name}, your DB connected....`)))("Himanshu");
