4309222aa81c641a9852da3fd113779c6ae3547e
Himanshu kumar <kh419368@gmail.com>
2025-08-24 23:40:46 +0530

    Refactor objectloop.js: Comment out unused array and loop examples for clarity

--- js-chaiaurcode/objectloop.js
+++ js-chaiaurcode/objectloop.js
@@ -10,9 +10,55 @@
 //     console.log(`${key} :- ${myobj[key]}`);
 //  }
 
-let arr=["javascript","python","java","cpp"];
+// const arr=["javascript","python","java","cpp"];
 
-for (const value in arr){
-    console.log(value);
-    // console.log(arr[value]);   
-}
+// for (const value in arr){
+//     console.log(value);
+//     console.log(arr[value]);   
+// }
+
+// arr.forEach(function (value,keys){
+//     console.log(value , keys);
+    
+// })
+
+// arr.forEach( (value)=>{
+//     console.log(value );
+    
+// })
+
+// function fun(items){
+//     console.log(items);
+    
+// }
+// arr.forEach(fun);
+
+// arr.forEach((items,index,arr)=>{
+//     console.log(items,index,arr);
+    
+// })
+
+
+let coding=[
+    {
+        language:`javascript`,
+        filename:`js`
+    },
+    {
+        language:`java`,
+        filename:`java`
+    },
+    {
+        language:`python`,
+        filename:`py`
+    },
+    {
+        language:`cpp`,
+        filename:`c++`
+    }
+]
+coding.forEach((value)=>{
+    console.log(`${value.language} :- ${value.filename}`);
+    // console.log(value.filename);
+    
+})
