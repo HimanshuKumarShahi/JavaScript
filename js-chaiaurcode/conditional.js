de71f1bd77d7cc3f7dbadc656dc010300ad37282
Himanshu kumar <kh419368@gmail.com>
2025-08-24 22:00:50 +0530

    Refactor IIFE, conditional, function, loops, and scopes files: Comment out unused code for clarity and organization

---
+++ js-chaiaurcode/conditional.js
@@ -0,0 +1,155 @@
+const num=2;
+
+// if(num==2)
+// {
+//     console.log("exexuted...");
+// }
+// else
+// {
+//     log("not exexuted...");
+// }
+
+
+// if(2 === 2) //also check data typesame or not
+// {
+//     console.log("exexuted...");
+// }
+// else
+// {
+//    console.log("not exexuted...");
+// }
+
+
+
+
+// const temp=9;
+// if(temp >=40){
+//     console.log("too hot...");
+    
+// }
+// else if(temp >=25){
+//     console.log("moderate ...");
+    
+// }
+// else {
+//     console.log("good to live ...");
+    
+// }
+
+
+// const score=100;
+// if(score > 150){
+//     console.log("you got fly power...");
+// }
+// else if(score > 90){
+//     console.log("you got super power...");
+// }   
+// else if(score > 80){
+//     console.log("you got god power...");
+// }   
+// else{
+//     console.log("you are weak...");
+// }   
+
+
+// const balance=2000;
+// if(balance <= 500){
+//     console.log("balance is lower than 500");
+    
+// }else if(balance <1000){
+//     console.log("balance is lower than 1000");
+    
+// }else if(balance < 1500){
+//     console.log("balnce is less than 1500");
+    
+// }
+// else {
+//     console.log("your balance is above 1500 ");
+    
+// }
+
+
+// const isloggedin=true;
+// const debitcard=true;
+// const googleloggedin=false;
+// const githubloggedin=true;
+
+// if(isloggedin && debitcard){
+//     console.log("you can buy the course...");
+    
+// }
+// if(googleloggedin || githubloggedin){
+//     console.log("user logged in ...");
+    
+// }
+
+// console.log("----switch case --------")
+
+// const month = 2;
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("february");
+        
+//         break;
+//     case 3:
+//         console.log("March");
+        
+//         break;
+//     case 4:
+//         console.log("April");
+        
+//         break;
+//     case 5:
+//         console.log("may");
+        
+//         break;
+
+//     default:
+//         console.log("default nothing here");
+        
+//         break;
+// }
+
+// const user=["hk","pk"];
+
+// if(user.length <=15){
+//     console.log(`your email id : ${user[0]}`);
+    
+// }
+// else {
+//     console.log(`your id is deleted...`);
+    
+// }
+
+
+// const steps={
+//     name:"himanshu",
+//     age:21
+// }
+
+// if(Object.keys(steps).length === 0){
+//     console.log(`object is empty ....`);
+    
+// }
+// else {
+//     console.log(Object.keys(steps).length);
+    
+// }
+
+
+// let val;
+// val=5??10;
+// let val2= null ?? 20;
+// console.log(val);
+// console.log(val2);
+
+
+// console.log("--------------");
+
+        //------------------------ condiion ? true:false---------------------------
+// let val1=10,val2=20;
+// console.log((val1>val2?val1:val2));
