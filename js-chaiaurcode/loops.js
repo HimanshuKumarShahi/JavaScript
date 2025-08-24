a15b0d16491e5d6b21a90084ec3c63e39114ad00
Himanshu kumar Shahi <himanshushahi2006@gmail.com>
2025-08-24 23:20:19 +0530

    learninf for loops ,of ,in
    detail learninfg of loops

---
+++ js-chaiaurcode/loops.js
@@ -0,0 +1,94 @@
+// for loop
+
+// for (let a = 1; a <= 10; a++) {
+//     const element = a;
+//     console.log(element);
+// }
+
+// for (let i = 1; i <=3; i++) {
+//    console.log(`outer loop value ${i}`);
+//    for(let j=1;j<=10;j++){
+//     console.log(`innerloop value j:${j} , outer loop i:${i} `);
+//     console.log( i + `*` + j + `=` + (i*j));
+    
+//    }
+// }
+
+// let arr=[23,78,99,45,345,98,765,0];
+//     console.log(arr.length);
+    
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);   
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 12){
+//         console.log(`the number is detected ${i}`);
+//         break;
+        
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 12){
+//         console.log(`the number is detected ${i}`);
+//         continue;
+        
+//     }
+//     console.log(i);
+// }
+
+// console.log(`------while loop ---------`);
+
+// let i=1;
+// while (i<=25) {
+//     console.log(`the numbers : ${i}`);
+//     i+=2;
+// }
+
+// let arr=["ram","sita","laxman","bharat","shatrughan"];
+// let i=0;
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i=i+1    
+// }
+
+// let i=20;
+// do{
+//     console.log(`The num is: ${i}`);
+//     i--;
+    
+// }while(i >= 0);
+
+// console.log(`----loops for,in,of ------`);
+
+// let arr=[2,6,45,7,87,9,3,76]
+
+// for (const value of arr) {
+//     console.log(value);
+// }
+
+// let word="javascript is best language";
+
+// for (const char of word){
+//     console.log(`the char is:${char.toUpperCase()}`);
+    
+// }
+
+// const map = new Map();
+// map.set(`IN`,`india`)
+// map.set(`USA`,`america`)
+// map.set(`uk`,`england`)
+// map.set(`fr`,`france`)
+
+// console.log(map);
+
+// for(const [id,value] of map){
+//     console.log(id + `:-` + value);
+    
+// }
+
+
---
+++ js-chaiaurcode/objectloop.js
@@ -0,0 +1,18 @@
+//  const myobj={
+//     name:"chai",
+//     experience:"4yrs",
+//     role:"developer",
+//     charges:"$400"
+//  }
+
+// for (const key in myobj){
+//     // console.log(`key is: ${myobj[key]}`);
+//     console.log(`${key} :- ${myobj[key]}`);
+//  }
+
+let arr=["javascript","python","java","cpp"];
+
+for (const value in arr){
+    console.log(value);
+    // console.log(arr[value]);   
+}
