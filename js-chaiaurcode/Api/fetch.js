a60d18acfa3f245f3501c17f7ef86c9fa095ff5c
Himanshu kumar <kh419368@gmail.com>
2025-09-06 16:16:28 +0530

    Remove commented-out code and clean up fetch implementation in promises.js and fetch.js

--- js-chaiaurcode/Api/fetch.js
+++ js-chaiaurcode/Api/fetch.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 fetch('https://api.github.com/users/Himanshukumarshahi')
     .then((responce) => {
         return responce.json();
@@ -10,17 +10,4 @@ fetch('https://api.github.com/users/Himanshukumarshahi')
 
     .catch(() => {
         console.log("error...");
-=======
-fetch('https://api.github.com/users/Himanshukumarshahi')
-    .then((responce) => {
-        return responce.json();
-    })
-    .then((data) => {
-        console.log(data);
-
-    })
-
-    .catch(() => {
-        console.log("error...");
->>>>>>> 6fdb8a8 (Save my local changes)
-    })
+    });
