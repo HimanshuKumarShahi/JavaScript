d3d38a06c6c8947a4962d88d4583f0569392ba65
Himanshu kumar Shahi <himanshushahi2006@gmail.com>
2025-09-06 15:57:09 +0530

    here learn fetch in js

---
+++ js-chaiaurcode/Api/fetch.js
@@ -0,0 +1,12 @@
+fetch('https://api.github.com/users/Himanshukumarshahi')
+    .then((responce) => {
+        return responce.json();
+    })
+    .then((data) => {
+        console.log(data);
+
+    })
+
+    .catch(() => {
+        console.log("error...");
+    })
