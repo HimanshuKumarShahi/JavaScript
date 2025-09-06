67bbc75a0c36fd496f92a4bd5f2f3f86c3fc5f0c
Himanshu kumar <himanshushahi2006@gmail.com>
2025-09-07 01:17:58 +0530

    Refactor prototypes.js to implement String prototype method for total length calculation

---
+++ js-chaiaurcode/classesobjects/prototypes.js
@@ -0,0 +1,62 @@
+// let name="Himanshu Kumar";
+// console.log(name.length);
+// console.log(name.truelength);
+
+
+// let hero=["thor","ironman","hulk"];
+
+// const power={
+//     thor:"hammer",
+//     ironman:"jarvis",
+//     hulk:"size increase",
+
+// heropower:function(){
+//     console.log(`the power of thre is: ${this.thor}`);
+    
+// }
+// }
+// Object.prototype.himanshu=function(){
+//     console.log("you have power to ruinde it");
+    
+// }
+// Array.prototype.greet=function(){
+//     console.log("hellow Dear !!!");
+    
+// }
+
+// power.himanshu();
+// power.greet();
+// hero.greet();
+
+                // old method 
+// const teacher={
+//     makevideo:true,
+//     project:javaScript
+// }
+// const user={
+//     name:"student1",
+//     email:"test@test"
+// }
+// const staff={
+//     fulltime:true,
+//     present:false,
+//     __proto__:teacher
+// }
+
+// teacher.__proto__=user;
+
+            // new method
+
+    // Object.setPrototypeOf(teacher,user);
+
+    let newuser="Hello Dear ";
+
+    String.prototype.totallength=function(){
+        // console.log(this);
+        // console.log(this.newuser);
+        console.log(this.trim().length);
+        
+    }
+    newuser.totallength();
+
+    "I LOVE JAVASCRIPT..".totallength();
