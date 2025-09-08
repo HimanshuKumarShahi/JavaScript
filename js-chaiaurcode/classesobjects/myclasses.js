cc6fd4ef242c5c5064c2f02cea32cfd37fd41df5
Himanshu kumar <himanshushahi2006@gmail.com>
2025-09-09 00:08:24 +0530

    work on classes also the behind the seen of functions and class

--- js-chaiaurcode/classesobjects/myclasses.js
+++ js-chaiaurcode/classesobjects/myclasses.js
@@ -1,17 +1,40 @@
-class user {
-    constructor(username,email,password) {
-        this.username=username
-        this.email=email
-        this.password=password
-    }
-    encrypt(){
-        return `a1${this.password}239986pceg`
-    }
-    addprefix(){
-        return `Mr/Mrs ${this.username.toUpperCase()} `
-    }
+// class user {
+//     constructor(username, email, password) {
+//         this.username = username
+//         this.email = email
+//         this.password = password
+//     }
+//     encrypt() {
+//         return `a1${this.password}239986pceg`
+//     }
+//     addprefix() {
+//         return `Mr/Mrs ${this.username.toUpperCase()} `
+//     }
+// }
+// const chai = new user("rohit", "as@12.com", '2398')
+// console.log(chai);
+// console.log(chai.encrypt());
+// console.log(chai.addprefix());
+
+
+// behind the seen
+
+function user(username, email, password) {
+    this.username = username
+    this.email = email
+    this.password = password
 }
-const chai=new user("rohit","as@12.com",'2398')
-console.log(chai);
-console.log(chai.encrypt());
-console.log(chai.addprefix());
+
+user.prototype.encrypt = function () {
+    return `a1${this.password}239986pceg`
+}
+user.prototype.addprefix = function () {
+     return `Mr/Mrs ${this.username.toUpperCase()} `
+}
+
+
+
+const data = new user("rohit", "as@12.com", '2398')
+console.log(data);
+console.log(data.encrypt());
+console.log(data.addprefix());
