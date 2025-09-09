14da84102e100ce3b54d9c13f56a40b1cbc2c3ae
Himanshu kumar <himanshushahi2006@gmail.com>
2025-09-10 00:10:30 +0530

    use of get or set to hide imp data

--- js-chaiaurcode/getter_setter.js
+++ js-chaiaurcode/getter_setter.js
@@ -3,8 +3,26 @@ class user{
     this.username=username
     this.email=email
     this.password=password
-}}
+}
+
+    get email(){
+        return `${this._email.toUpperCase()} :=is change into upper case `
+    }
+    set email(value){
+        this._email=value
+    }
+    get password(){
+        return this._password.toUpperCase();
+    }
+    set password(value){
+         this._password=value.toUpperCase();
+    }
+}
 
 
-const newuser=new user("raja",'raj@.com','0987');
+const newuser=new user("raja",'raj@.com','ds67y')
 console.log(newuser.password);
+console.log(newuser.email);
+
+
+
