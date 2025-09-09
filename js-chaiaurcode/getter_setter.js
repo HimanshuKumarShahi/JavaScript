ee25a7d3094af0b7cc4f59e1ef9d4c6541c44365
Himanshu kumar <himanshushahi2006@gmail.com>
2025-09-09 23:49:10 +0530

    getter_setter working

---
+++ js-chaiaurcode/getter_setter.js
@@ -0,0 +1,10 @@
+class user{
+    constructor(username,email,password){
+    this.username=username
+    this.email=email
+    this.password=password
+}}
+
+
+const newuser=new user("raja",'raj@.com','0987');
+console.log(newuser.password);
