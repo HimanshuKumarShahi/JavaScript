a525fa841b1b1c265bd1dd52cdc9b8b6861d4abb
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-10-30 20:43:44 +0530

    function update

--- js-chaiaurcode/basics/function.js
+++ js-chaiaurcode/basics/function.js
@@ -1,32 +1,36 @@
 
-// function name(){
-//     console.log("Apple");
-//     console.log("Ball");
+function name(){
+    console.log("Apple");
+    console.log("Ball");
     
-// }
-// name();
-// function sum(a,b){
-//     return a+b;
-// }
-// console.log("The sum of two number is:_",sum(3,5));
-// console.log(sum(3,"5"));
-// console.log(sum(3,"a"));
+}
+name();
+function sum(a,b){
+    return a+b;
+}
+console.log("The sum of two number is:_",sum(3,5));
+console.log(sum(3,"5"));
+console.log(sum(3,"a"));
 
-// function userlogin(username,userid){
-//     if(username===undefined || userid===undefined){
-//         return "please enter valid user name and id";
-//     }
-//     else
-//     return `hello dear ${username}, you are logged in and your id is:${userid}`;
-// }
-// function userlogin(username="tony"){
-//     if(!username ){
-//         return "please enter valid user name and id";
-//     }
-//     else
-//     return `hello dear ${username}, you are logged in `;
-// }
-// console.log(userlogin());   
+function userlogin(username,userid){
+    if(username===undefined || userid===undefined){
+        return "please enter valid user name and id";
+    }
+    else
+    return `hello dear ${username}, you are logged in and your id is:${userid}`;
+}
+function userlogin(username="tony"){
+    if(!username ){
+        return "please enter valid user name and id";
+    }
+    else
+    return `hello dear ${username}, you are logged in `;
+}
+console.log(userlogin());   
+
+
+
+
 
 function carttotal(...num){
     return num;
