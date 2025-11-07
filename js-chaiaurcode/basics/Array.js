b481357c147537c15567230642c59f1250307be9
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-11-07 20:36:09 +0530

    littel update in array section

--- js-chaiaurcode/basics/Array.js
+++ js-chaiaurcode/basics/Array.js
@@ -26,27 +26,27 @@
 
 // console.log("c",arr2);
 
-// const hero=["ironman","captionamerica","thor","hulk"]
-// console.log(hero);
-// const newhero=["spiderman","blackpanther","doctorstrange"]
-// console.log(newhero);
+const hero=["ironman","captionamerica","thor","hulk"]
+console.log(hero);
+const newhero=["spiderman","blackpanther","doctorstrange"]
+console.log(newhero);
 
-// console.log(hero.push(newhero))
-// const allhero=(hero.concat(newhero))
-// console.log(allhero);
+console.log(hero.push(newhero))
+const allhero=(hero.concat(newhero))
+console.log(allhero);
 
-// const all_new_hero=[...hero,...newhero] 
-// console.log(all_new_hero)
+const all_new_hero=[...hero,...newhero] 
+console.log(all_new_hero)
 
-// const number=[2,5,[23,67,45,33],34,8,56,4,[56,78,[345,677,899]]]
-// console.log(number);
-// const flatnumber=number.flat(Infinity);
-// console.log(flatnumber);
+const number=[2,5,[23,67,45,33],34,8,56,4,[56,78,[345,677,899]]]
+console.log(number);
+const flatnumber=number.flat(Infinity);
+console.log(flatnumber);
 
-// console.log(Array.isArray("himanshu"));
-// console.log(Array.from("himanshu"));
+console.log(Array.isArray("himanshu"));
+console.log(Array.from("himanshu"));
 
-// let score1=100;
-// let score2=200;
-// let score3=300;
-// console.log(Array.of(score1,score2,score3));
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
